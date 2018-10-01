f9ab4de17ae3802a45480c048c552cc8ad8458a2
NathanDrain <31935495+NathanDrain@users.noreply.github.com>
2018-10-01 15:26:08 -0700

    Updated interface and routing files
    Updated library_webservice interface with functions for endpoints and corresponding helper functions in db_routing.

--- db_routing.py
+++ db_routing.py
@@ -2,24 +2,85 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-#set uri for book db location.
-# resources books users book collections
 
-# get functions - can we parse the http parameters?
-def get_resource(resource, kwargs):
-    return
-
-# post functions - creates new records for resources
-def post_resource(resource, kwargs):
-    return
-
-# put functions - update an instance of a resource, updates entire record, not just attributes.
-def put_resource(resource, kwargs):
+# Function to query by received parameters.
+# returns a dictionary of query results.
+def get_books(parameters):
+    # valid parameters check.
+    # string together for valid sql query.
     return
 
 
-# del delete instance of a resource
-def delete_resource(resource, kwargs):
+# Function to retrieve record of a single book
+def get_books(record):
     return
 
-#
+
+# Function to query Users by received parameters.
+def get_users(parameters):
+    return
+
+
+# Function to get single user by record number.
+def get_users(record):
+    return
+
+
+# Post
+# function to post new book.
+def post_books(json_obj):
+    # valid json fields check
+    return
+
+
+# function to post new user
+def post_users(json_obj):
+    # valid json fields check
+    return
+
+
+# Put
+# function to update books based on http query string and json object
+def put_books(parameters, json_obj):
+    # check valid json object, check valid parameters
+    return
+
+
+# function to update single record with json object
+def put_books(record, json_obj):
+    # check valid record valid json
+    return
+
+
+# function to update users based on http query string and json object
+def put_users(parameters, json_obj):
+    # check valide parameters, json object
+    return
+
+
+# function to update single user with json object info
+def put_users(record, json_obj):
+    # check valid record, valid json object
+    return
+
+
+# DELETE
+# delete books according to http query parameters.
+def delete_books(parameters):
+    return
+
+
+# delete specific record for book.
+# what happens to all other primary key#?
+def delete_books(record):
+    return
+
+
+# delete users based on http query parameters.
+def delete_users(parameters):
+    return
+
+
+# delete single record user.
+def delete_users(record):
+    return
