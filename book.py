3fd4abc02168d5ca504452f509e3c0618c756d9f
sarahxzhang20 <zhang.xinyuan@husky.neu.edu>
2018-10-02 15:39:42 -0700

    classes added

---
+++ book.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+
+# Book class
+class Book:
+    def __init__(self, book_id, author_first, author_last, title, publish_date, genre, subject, loaned, due_date,
+                 borrower):
+        self.book_id = book_id
+        self.author_first = author_first
+        self.author_last = author_last
+        self.title = title
+        self.publish_date = publish_date
+        self.genre = genre
+        self.subject = subject
+        self.loaned = loaned
+        self.due_date = due_date
+
