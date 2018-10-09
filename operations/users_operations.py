6aebc72da764bc24e79f0a4cf3f1ad87b8f3d213
kaschu <schultz.kat@husky.neu.edu>
2018-10-09 15:56:40 -0700

    Users endpoint python file added

---
+++ operations/users_operations.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, jsonify
+
+# this allows for a string query
+# GET - returns all users
+# POST - add a user
+# PUT - bulk update user records
+# DELETE - bulk delete users
+# users? <param> = specify query parameters.
+@app.route('/users', methods=['GET', 'POST', 'PUT', 'DELETE'])
+def users():
+    # error checking here for valid request.
+        parameters = request.args
+        json_obj = request.json
+
+# this does not allow for a string query
+# GET - returns specific user
+# POST - N/A
+# PUT - edit user
+# DELETE - remove user
+@app.route('/users/<user>', methods=['GET', 'PUT', 'DELETE'])
+def access_user(user):
+    json_obj = request.json
