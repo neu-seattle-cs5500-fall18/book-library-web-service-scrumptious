ca36be980e3907b12f9b1b4fb28d7dc6b24504c3
NathanDrain <31935495+NathanDrain@users.noreply.github.com>
2018-10-13 10:34:45 -0700

    updating books endpoint

--- books.py
+++ books.py
@@ -1,8 +1,11 @@
 from flask import Flask
-from flask_restplus import Api, fields, Resource
+from flask_restplus import Api, fields, Resource, reqparse
+
+from books_queries import query_book_params
 
 app = Flask(__name__)
 api = Api(app)
+
 
 # Need Parameter Checking, where to store valid parameters, error list.
 # restplus automatically returns json object type.
@@ -13,24 +16,43 @@ book_model = api.model('BookModel', {
     'title': fields.String('The book title.'),
     'author_first_name': fields.String('The author\'s first name.'),
     'author_last_name': fields.String('The author\'s last name.'),
+    #Author needs updateed to accomodate several authors
+    'publish_date': fields.String('The publish date of a book'),
+    'subject': fields.String('Subject, such as "science", "Reference", "Non-Fiction"'),
+    'genre': fields.String('Genre classification for a fiction book (i.e. horror, science fiction'),
+    'loaned_out': bool('If a book is on loan true, false otherwise'),
+    'notes': list('List of personal notes about a book'),
+    'collections': list('List of collections book belongs to')
 })
+
+query_parser = reqparse.RequestParser()
+query_parser.add_argument('title', 'author_first_name', 'author_last_name', 'publish_date', 'subject', 'genre')
 
 
 class Book(object):
-    def __init__(self, book_id, title, author_first_name, author_last_name):
+    def __init__(self, book_id, title, author_first_name, author_last_name, publish_date, subject, genre, loaned_out,
+                 notes, collections):
         self.book_id = book_id
         self.title = title
         self.author_first_name = author_first_name
         self.author_last_name = author_last_name
+        self.publish_date = publish_date
+        self.subject = subject
+        self.genre = genre
+        self.loaned_out = loaned_out
+        self.notes = notes
+        self.collections = collections
 
         self.status = 'active'
 
 
 @api.route('/books', endpoint='books')
 class Books(Resource):
-    @api.marshal_with(book_model)
-    @api.response(200, 'Resource successfully gotten')
-    # Returns all objects of resource
+    @api.response(400, 'Invalid query')
+    @api.response(200, 'Resource gotten')
+    #parser = api.parser()
+    #parser.add_argument()
+
     def get(self):
         """
         Gets all books within resource Books.
@@ -41,59 +63,71 @@ class Books(Resource):
             4, "The Old Man and the Sea", "George r.r.", "Martin"
         )
         books.append(book)
+        args = query_parser.parse_args()
+        result = query_book_params(args)
         return books
 
-    # Add new record to resource- require json receipt
-    # 201 status for created
-    @api.expect(book_model)
-    @api.response(201, 'Resource created')
+
+
+    @api.response(400, 'Validation error')
+    @api.marshal_with(book_model, code=201, description='Object created')
     def post(self):
         """
-        Creates a new book record.
+        Creates a new book record for a single book.
         :return: JSON of created record.
         """
+        books = []
+        books.append(self)
+
         # Query routing function here
         # set response code 201
-        return "Success", 201
+        return "Successfully Created ID " + books.pop()
 
 
-
-
-@api.route('/books/notes/', endpoint='books')
-@api.doc(params={'book_id': 'A record for a book'})
+@api.route('/books/<book_id>', endpoint='books', doc = {'params':{'book_id': 'Record identifier for a book'}})
 class BookRecord(Resource):
-    @api.response(404, "Incorrect record")
+    @api.response(404, 'Incorrect record ')
+    @api.response(200, 'Resource gotten')
     def get(self, book_id):
         """
         Gets a specific book record based on book_id.
         :param book_id: Record of a book.
-        :return: JSON of record.
+        :return: JSON of requested book record.
         """
-        return "success", 200
+        return "Successfully got %s " %book_id
 
-    @api.marshal_with(book_model)
     @api.response(404, "Incorrect record")
+    @api.marshal_with(book_model, code='200', description='Object amended')
     def put(self, book_id):
         """
-        Updates an existing record.
+        Updates an existing record  based on book_id.
         :param book_id: Record number to be updated.
-        :return: Json of updated record.
+        :return: Json with book_id of updated record.
         """
-        return "success again", 200
+        return "Successfully updated %s " %book_id
 
+    @api.response(200, 'Objected deleted')
     @api.response(404, "No such record")
     def delete(self, book_id):
         """
-        Delete a book record based on book_id
+        Delete a book record based on book_id.
         :param book_id: Record to be deleted.
-        :return:
+        :return: Json of book_id of deleted record.
         """
-        return "Successfully Deleted", 200
+        return "Successfully deleted %s " %book_id
 
 
-@api.route('/books/collections/', endpoint='books')
+@api.route('/books/notes', endpoint='books')
+@api.doc(params={'book_id': 'A record for a book'})
+class BookNotes(Resource):
+        def get(self):
+            return
+
+
+
+@api.route('/books/collections', endpoint='books')
 @api.doc(params={'epoch_start': 'Earliest publish date', 'epoch_end': 'Latest publish date'})
-class TimeFrame(Resource):
+class BookCollections(Resource):
     def get(self, epoch_start, epoch_end):
         """
         Query books by publish date after start date and before end date.
---
+++ books_queries.py
@@ -0,0 +1,4 @@
+
+
+def query_book_params(args):
+    return
