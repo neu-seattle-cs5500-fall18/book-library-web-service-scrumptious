54a84746853b27208bd0196e2a7637c030ca1640
Anirudh Mallya <mallya.a@husky.neu.edu>
2018-11-01 22:01:05 -0700

    reverted back files

--- apis/collections_api.py
+++ apis/collections_api.py
@@ -1,6 +1,4 @@
-from flask import request
 from flask_restplus import Namespace, Resource, fields
-from model.collection_dao import get_all_collections, create_new_collection,add_book_to_collection_id, delete_book_from_collection_id, get_collection, delete_collection
 
 api = Namespace('collections', 'Book collections operations')
 
@@ -12,87 +10,21 @@ collection_marshaller = api.model('BookCollections', {
 })
 
 
-@api.route('')
+@api.route('/bookcollections', endpoint='bookcollections')
 class BookCollections(Resource):
-    @api.marshal_with(collection_marshaller, code=200)
+    @api.marshal_with(collection_marshaller)
+    @api.response(200, 'Resource successfully gotten')
     def get(self):
         """
-        Gets all users
-        :return: Json object of all users
+        Gets all collections of books
+        :return: Json object of all collections of books
         """
-        print('Received GET on resources /users')
-        response = get_all_collections()
+        book_col = []
+        book = BookCollection(
+            44, 1, "Harry"
+        )
+        book_col.append(book)
+        return book_col
 
-        return response
-
-    @api.response(400, 'Record not found')
-    @api.response(201, 'Created new collection.')
-    @api.expect(collection_marshaller)
-    def post(self):
-        """
-        Creates a new book collection.
-        :return: collection ID of the created record.
-        """
-        print('Received POST on resource /collections')
-
-        collection_info = request.get_json()
-        response = create_new_collection(collection_info)
-
-        return response, 201
-
-
-@api.route('/<collection_id>', endpoint='book_collections')
-@api.doc(params={'collection_id': 'An ID for a collection record'})
-class BookCollectionRecord(Resource):
-    @api.marshal_with(collection_marshaller)
-    def get(self, collection_id):
-        print('Received GET on collection /collections/<collection_id>')
-
-        collection_record = get_collection(collection_id)
-
-        return collection_record
-
-    @api.route('/<collection_id>/add/<book_id>', endpoint='book_collections')
-    @api.response(code=400, description='Record not found')
-    @api.response(code=200, description='Success')
-    def put(self, collection_id, book_id):
-        """
-        Updates an existing collection record based on collection_id.
-        :param collection_id: Record number to be updated.
-        :return: Json with collection_id of updated record.
-        """
-        print('Received PUT on resource /collections/<collection_id>/<book_id>')
-
-        collection_id = add_book_to_collection_id(collection_id, book_id)
-
-        return collection_id
-
-    @api.route('/<collection_id>/delete/<book_id>', endpoint='book_collections')
-    @api.response(code=400, description='Record not found')
-    @api.response(code=200, description='Success')
-    def put(self, collection_id, book_id):
-        """
-        Updates an existing collection record based on collection_id.
-        :param collection_id: Record number to be updated.
-        :return: Json with collection_id of updated record.
-        """
-        print('Received PUT on resource /collections/<collection_id>/<book_id>')
-
-        collection_id = delete_book_from_collection_id(collection_id, book_id)
-
-        return collection_id
-
-    @api.response(code=200, description='Collection deleted')
-    def delete(self, collection_id):
-        """
-        Delete a collection based on collection_id.
-        :param collection_id: Collection to be deleted.
-        :return: Json of collection_id of deleted record.
-        """
-        print('Received DELETE on resource /collections/<collection_id>')
-
-        id_of_deleted_collection = delete_collection(collection_id)
-
-        return id_of_deleted_collection
 
 
--- model/collection.py
+++ model/collection.py
@@ -2,9 +2,8 @@ from library_webservice import db
 from model import book
 
 
-class BookCollection(db.Model):
-    collection_id: db.Column(db.Integer, primary_key=True)
-    book_ids: db.Column('book_id', backref='Book', lazy=True)
-    title: db.Column(db.String)
-    is_deleted = db.Column(db.Boolean, nullable=False, default=False)
+# class BookCollection(db.Model):
+#     collection_id: db.Column(db.Integer, primary_key=True)
+#     book_id: db.Column('book_id', backref='Book', lazy=True)
+#     title: db.Column(db.String)
 
