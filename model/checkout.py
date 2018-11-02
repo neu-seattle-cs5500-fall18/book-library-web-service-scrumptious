e02208651bfb1b398eabcdb2306847e828bc602c
DDDN1212 <drain.n@husky.neu.edu>
2018-11-02 16:35:54 -0700

    updated book

--- model/checkout.py
+++ model/checkout.py
@@ -10,3 +10,13 @@ class Checkout(db.Model):
     return_date = db.Column(db.Date, nullable=True)
     is_deleted = db.Column(db.Boolean, nullable=False, default=False)
 
+    def __init__(self, checkout_id, user_id, book_id, checkout_date, due_date, return_date):
+        self.checkout_id = checkout_id
+        self.user_id = user_id
+        self.book_id = book_id
+        self.checkout_date = checkout_date
+        self.due_date = due_date
+        self.return_date = return_date
+
+
+
