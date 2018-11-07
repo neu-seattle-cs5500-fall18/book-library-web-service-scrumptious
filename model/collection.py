23e61bb00dd07236f3844479a9b02bbca25e2773
Anirudh Mallya <mallya.a@husky.neu.edu>
2018-11-06 17:51:36 -0800

    collections latest

--- model/collection.py
+++ model/collection.py
@@ -1,9 +1,9 @@
 from library_webservice import db
-from model import book
 
 
-# class BookCollection(db.Model):
-#     collection_id: db.Column(db.Integer, primary_key=True)
-#     book_id: db.Column('book_id', backref='Book', lazy=True)
-#     title: db.Column(db.String)
+class BookCollection(db.Model):
+    collection_id = db.Column(db.Integer, primary_key=True)
+    book_ids = db.Column(db.String, nullable=False)
+    title = db.Column(db.String, nullable=False)
+    is_deleted = db.Column(db.Boolean, nullable=False, default=False)
 
