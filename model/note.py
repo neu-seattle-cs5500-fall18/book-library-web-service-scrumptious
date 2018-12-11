2c991522a871b373d7e8818dbbeefac5a43b3542
kaschu <schultz.kat@husky.neu.edu>
2018-12-11 15:35:53 -0800

    copy and note documentation finished

--- model/note.py
+++ model/note.py
@@ -10,15 +10,24 @@ class Note(db.Model):
         return '<Note %r : %r %r>' %(self.note_title, self.note, self.book_id)
 
     def to_dict(self):
+        """
+        Method to convert a Note object to a dict object representation
+        :return: dict representation of a Note
+        """
         a_dict = {
             'note_title': self.note_title,
             'note': self.note,
-            'book_id':self.book_id
+            'book_id': self.book_id
         }
 
         return a_dict
 
     def update(self, **kwargs):
+        """
+        Method to update a Note's attributes
+        :param kwargs: A dict of valid key-value pairs
+        :return: None
+        """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
